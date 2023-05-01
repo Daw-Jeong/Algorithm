26aeea84c69b5820f0d29a9f8c213ebdab93af31
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-01 16:36:04 +0900

    [unrated] Title: 문자열 섞기, Time: 17.09 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/README.md
+++ 프로그래머스/unrated/181942. 문자열 섞기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자열 섞기 - 181942 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=java) 
 
 ### 성능 요약
 
-메모리: 74.8 MB, 시간: 12.86 ms
+메모리: 79.6 MB, 시간: 17.09 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,14 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        char[] charArr1 = str1.toCharArray();
-        char[] charArr2 = str2.toCharArray();
+        // char[] charArr1 = str1.toCharArray();
+        // char[] charArr2 = str2.toCharArray();
         
+        // for (int i = 0; i < str1.length(); i++) {
+        //     answer = answer + charArr1[i] + charArr2[i];
+        // }
         for (int i = 0; i < str1.length(); i++) {
-            answer = answer + charArr1[i] + charArr2[i];
+            answer = answer + str1.charAt(i) + str2.charAt(i);
         }
         return answer;
     }
