61abf6ce9da8c963f8755299a06085ae7565ffa4
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-04 20:39:23 +0900

    [unrated] Title: 카운트 업, Time: 2.08 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,11 +1,13 @@
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[end-start+1];
+//         int[] answer = new int[end-start+1];
 
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = start;
-            start++;
-        }
-        return answer;
+//         for (int i = 0; i < answer.length; i++) {
+//             answer[i] = start;
+//             start++;
+//         }
+//         return answer;
+        return IntStream.rangeClosed(start, end).toArray();
     }
 }
