21088f98b60ab8e64174eb24ec4e3dfe282cee1e
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-22 11:22:38 +0900

    [unrated] Title: 가까운 1 찾기, Time: 2.71 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,9 +1,12 @@
+import java.util.stream.*;
 class Solution {
     public int solution(int[] arr, int idx) {
-        int answer = -1;
-        for (int i = idx; i < arr.length; i++) {
-            if (arr[i] == 1) return i;
-        }
-        return answer;
+        // int answer = -1;
+        // for (int i = idx; i < arr.length; i++) {
+        //     if (arr[i] == 1) return i;
+        // }
+        // return answer;
+        
+        return IntStream.range(idx, arr.length).filter(i -> arr[i] == 1).findFirst().orElse(-1);
     }
 }
