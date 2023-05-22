71459ea6358aecd85f71e753a48d82d092edfcb5
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-23 01:16:15 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,22 @@
+import math
 def solution(numer1, denom1, numer2, denom2):
+#     tempNumer = numer1 * denom2 + numer2 * denom1
+#     tempDenom = denom1 * denom2
+
+#     for i in range(denom1 * denom2, 1, -1):
+#         if tempNumer % i == 0 and tempDenom % i == 0 :
+#             tempNumer /= i
+#             tempDenom /= i       
+            
+#     answer = [tempNumer, tempDenom]
+    
+#     return answer
+
     tempNumer = numer1 * denom2 + numer2 * denom1
     tempDenom = denom1 * denom2
-
-    for i in range(denom1 * denom2, 1, -1):
-        if tempNumer % i == 0 and tempDenom % i == 0 :
-            tempNumer /= i
-            tempDenom /= i       
-            
-    answer = [tempNumer, tempDenom]
+    
+    temp = math.gcd(tempNumer, tempDenom)
+    answer = [tempNumer/temp, tempDenom/temp]
     
     return answer
+    
