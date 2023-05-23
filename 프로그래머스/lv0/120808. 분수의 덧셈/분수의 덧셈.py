e95cd13ec5c7fc74947e34f2ef14b4a687239358
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-23 11:20:39 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,17 +1,22 @@
-import math
-def solution(numer1, denom1, numer2, denom2):
-    tempNumer = numer1 * denom2 + numer2 * denom1
-    tempDenom = denom1 * denom2
+from fractions import Fraction
+def solution(num1, denom1, num2, denom2):
+    answer = Fraction(num1, denom1) + Fraction(num2, denom2)
+    return [answer.numerator, answer.denominator]
 
-    for i in range(denom1 * denom2, 1, -1):
-        if tempNumer % i == 0 and tempDenom % i == 0 :
-            tempNumer /= i
-            tempDenom /= i
-            break
+# import math
+# def solution(numer1, denom1, numer2, denom2):
+#     tempNumer = numer1 * denom2 + numer2 * denom1
+#     tempDenom = denom1 * denom2
+
+#     for i in range(denom1 * denom2, 1, -1):
+#         if tempNumer % i == 0 and tempDenom % i == 0 :
+#             tempNumer /= i
+#             tempDenom /= i
+#             break
             
-    answer = [tempNumer, tempDenom]
+#     answer = [tempNumer, tempDenom]
     
-    return answer
+#     return answer
 
 #     tempNumer = numer1 * denom2 + numer2 * denom1
 #     tempDenom = denom1 * denom2
