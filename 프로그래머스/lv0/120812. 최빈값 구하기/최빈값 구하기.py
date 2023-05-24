80d96873a4d55e401756ac5ccd274ed506feb486
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-24 19:25:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,38 @@
+from collections import Counter
+def solution(array):
+    cnt = Counter(array).most_common()
+    if len(cnt) == 1:
+        return cnt[0][0]
+    if cnt[0][1] == cnt[1][1]:
+        return -1
+    return cnt[0][0]
+    
+    
+#     array.sort()
+#     count_arr = {}
+#     count = 0
+    
+#     if len(array) == 1:
+#         return array[0]
+    
+#     for i in range(0, len(array)):
+#         if i >= 1:
+#             if array[i] == array[i-1]:
+#                 continue
+#         count_arr[array[i]] = array.count(array[i])
+
+#     print(count_arr)
+
+#     max_value = max(count_arr.values())
+#     print(max_value)
+    
+#     for j in count_arr:
+#         if count_arr[j] == max_value:
+#             count += 1
+            
+#     if count >= 2:
+#         return -1
+#     else:
+#         for n in count_arr:
+#             if count_arr[n] == max_value:
+#                 return count_arr[n]
