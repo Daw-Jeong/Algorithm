465eb5bfe22443f0e21aa11b614bd3c103d55edd
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-25 15:20:27 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,11 +1,18 @@
-import math
+# import math
 def solution(price):
-    answer = price
-    if price >= 500000:
-        answer = price * 0.8
-    elif price >= 300000:
-        answer = price * 0.9
-    elif price >= 100000:
-        answer = price * 0.95
+    price_rates = {500000:0.8, 300000:0.9, 100000:0.95, 0:1}
+    for price_range, rate in price_rates.items():
+        if price >= price_range: 
+            price *= rate
+            return int(price)
+
+#     answer = price
+#     if price >= 500000:
+#         answer = price * 0.8
+#     elif price >= 300000:
+#         answer = price * 0.9
+#     elif price >= 100000:
+#         answer = price * 0.95
     
-    return math.trunc(answer)
+#     # return math.trunc(answer)
+#     return int(answer)
