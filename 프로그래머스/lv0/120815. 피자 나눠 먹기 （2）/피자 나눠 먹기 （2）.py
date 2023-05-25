00dbb5441e672dc4b936ecd96a5142691a3f28ce
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-25 14:16:00 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,6 +1,10 @@
+import math
+
 def solution(n):
-    answer = 1
-    while 6 * answer % n != 0:
-        answer += 1
+#     answer = 1
+#     while 6 * answer % n != 0:
+#         answer += 1
     
-    return answer
+#     return answer
+
+    return (n * 6) // math.gcd(n, 6) // 6
