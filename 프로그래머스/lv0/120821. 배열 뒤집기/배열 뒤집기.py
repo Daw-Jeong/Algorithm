5e1c86baf744fa27133eaadc84f552438f566016
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-26 15:39:35 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,5 +1,7 @@
 def solution(num_list):
-    answer = []
-    for i in range(0, len(num_list)):
-        answer.append(num_list[len(num_list)-1-i])
-    return answer
+    # answer = []
+    # for i in range(0, len(num_list)):
+    #     answer.append(num_list[len(num_list)-1-i])
+    # return answer
+    
+    return num_list[::-1]
