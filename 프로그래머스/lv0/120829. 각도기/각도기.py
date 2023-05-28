f6c31ed69645a822855ca12d2b8ff3792d2d8fce
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-29 00:28:19 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.py
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -1,8 +1,11 @@
 def solution(angle):
-    angles = {180:4, 91:3, 90:2, 0:1}
+    return (angle // 90) * 2 + (angle % 90 != 0) * 1
     
-    for angleRange, val in angles.items():
-        if angle >= angleRange: return val
+    
+#     angles = {180:4, 91:3, 90:2, 0:1}
+    
+#     for angleRange, val in angles.items():
+#         if angle >= angleRange: return val
         
     # if angle == 180:
     #     return 4
