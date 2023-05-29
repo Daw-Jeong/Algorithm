6c327d80adb3f71f977c45fed43dab769f474fba
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-29 16:40:10 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
@@ -1,15 +1,17 @@
 def solution(hp):
-    answer = 0
     
-    if hp >= 5:
-            answer += hp // 5
-            hp %= 5
+    return hp // 5 + hp % 5 // 3 + hp % 5 % 3
+#     answer = 0
+    
+#     if hp >= 5:
+#             answer += hp // 5
+#             hp %= 5
             
-    if hp >= 3:
-            answer += hp // 3
-            hp %= 3
+#     if hp >= 3:
+#             answer += hp // 3
+#             hp %= 3
             
-    if hp >= 1:
-            answer += hp // 1
+#     if hp >= 1:
+#             answer += hp // 1
             
-    return answer
+#     return answer
