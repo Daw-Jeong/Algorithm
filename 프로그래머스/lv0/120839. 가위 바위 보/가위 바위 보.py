f015514aea77c210e359aa4f2cfe82ffe611f268
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-29 21:27:40 +0900

    [level 0] Title: 가위 바위 보, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,6 +1,14 @@
 def solution(rsp):
-    dic = {'2':'0','0':'5', '5':'2'}
-    return ''.join([dic[rsp[i]] for i in range(len(rsp))])
+    rsp = rsp.replace('2', 's')
+    rsp = rsp.replace('0', 'r')
+    rsp = rsp.replace('5', 'p')
+    rsp = rsp.replace('s', '0')
+    rsp = rsp.replace('r', '5')
+    rsp = rsp.replace('p', '2')
+    return rsp
+
+    # dic = {'2':'0','0':'5', '5':'2'}
+    # return ''.join([dic[rsp[i]] for i in range(len(rsp))])
 
 #     for i in range(len(rsp)):
 #         if rsp[i] == '2':
