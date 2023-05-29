10cef9cd671a586f08457670b54d9aff3221c894
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 01:55:55 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,12 +1,16 @@
 def solution(num_list, n):
     answer = []
-    order = 0
     
-    for i in range(len(num_list)//n):
-        answer.append([])
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
+    
+#     order = 0
+    
+#     for i in range(len(num_list)//n):
+#         answer.append([])
         
-        for j in range(n):
-            answer[i].append(num_list[order])
-            order += 1
+#         for j in range(n):
+#             answer[i].append(num_list[order])
+#             order += 1
         
     return answer
