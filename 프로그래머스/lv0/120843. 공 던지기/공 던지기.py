04cbcdd6e3d2c9a7b137fc9c0fe3d76ee60eaca4
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 10:37:12 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,9 +1,11 @@
 def solution(numbers, k):
 
-    answer = -1
-    for i in range(1, k+1):
-        answer += 2
-        if answer > len(numbers):
-            answer -= len(numbers)
+#     answer = -1
+#     for i in range(1, k+1):
+#         answer += 2
+#         if answer > len(numbers):
+#             answer -= len(numbers)
         
-    return answer
+#     return answer
+
+    return 2 * (k - 1) % len(numbers) + 1
