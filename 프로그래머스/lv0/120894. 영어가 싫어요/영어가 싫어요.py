b0055e584252d5a1a31a9d2bcfe5e61025aee167
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 15:51:08 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    to_num = ''
+    
+    while len(numbers) > 0:
+        for num in dic:
+            if numbers.startswith(num):
+                to_num += dic[num]
+                numbers = numbers[len(num):]
+                
+    return int(to_num)
