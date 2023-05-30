2b3eb4965cbba9130aef4df4db23e4ea32df7c53
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 11:41:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,18 +1,20 @@
 def solution(numbers, direction):
-    answer = list(range(len(numbers)))
+#     answer = list(range(len(numbers)))
 
-    if direction == 'right':
-        for i in range(len(numbers)):
-            if i == len(numbers) - 1:
-                answer[0] = numbers[i]
-            else:
-                answer[i + 1] = numbers[i]
+#     if direction == 'right':
+#         for i in range(len(numbers)):
+#             if i == len(numbers) - 1:
+#                 answer[0] = numbers[i]
+#             else:
+#                 answer[i + 1] = numbers[i]
                 
-    else:
-        for i in range(len(numbers)):
-            if i == 0:
-                answer[len(numbers) - 1] = numbers[i]
-            else:
-                answer[i - 1] = numbers[i]
+#     else:
+#         for i in range(len(numbers)):
+#             if i == 0:
+#                 answer[len(numbers) - 1] = numbers[i]
+#             else:
+#                 answer[i - 1] = numbers[i]
                 
-    return answer
+#     return answer
+
+    return [numbers[-1]] + numbers[:-1] if direction == 'right' else numbers[1:] + [numbers[0]]
