5262bcf0cf0692b1fcfc63dda1ce568d769babfc
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 17:23:57 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,3 +1,4 @@
 def solution(my_string):
 
-    return sorted([int(i) for i in my_string if i in "0123456789"])
+    # return sorted([int(i) for i in my_string if i in "0123456789"])
+    return sorted([int(i) for i in my_string if i.isdigit()])
