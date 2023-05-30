89a2342e91fcc58af85ca8bc40482b8337b486c7
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 14:52:08 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,4 +1,5 @@
 def solution(n):
-    if (n ** 0.5 == int(n ** 0.5)):
-        return 1
-    else: return 2
+    # if (n ** 0.5 == int(n ** 0.5)):
+    #     return 1
+    # else: return 2
+    return 1 if n ** 0.5 == int(n ** 0.5) else 2
