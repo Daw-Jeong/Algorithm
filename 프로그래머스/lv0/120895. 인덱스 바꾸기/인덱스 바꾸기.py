2295316b16256bddc943e67705e298e156a33afe
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 17:18:22 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,3 +1,8 @@
 def solution(my_string, num1, num2):
 
-    return my_string[:num1] + my_string[num2] + my_string[num1+1:num2] + my_string[num1] + my_string[num2+1:]
+    # return my_string[:num1] + my_string[num2] + my_string[num1+1:num2] + my_string[num1] + my_string[num2+1:]
+    
+    strlist = list(my_string)
+    strlist[num1], strlist[num2] = strlist[num2], strlist[num1]
+    
+    return ''.join(strlist)
