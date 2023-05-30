03b41ed4727f41f5a4cf6a6f461b55faa6963e47
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 11:22:49 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,18 @@
+def solution(numbers, direction):
+    answer = list(range(len(numbers)))
+
+    if direction == 'right':
+        for i in range(len(numbers)):
+            if i == len(numbers) - 1:
+                answer[0] = numbers[i]
+            else:
+                answer[i + 1] = numbers[i]
+                
+    else:
+        for i in range(len(numbers)):
+            if i == 0:
+                answer[len(numbers) - 1] = numbers[i]
+            else:
+                answer[i - 1] = numbers[i]
+                
+    return answer
