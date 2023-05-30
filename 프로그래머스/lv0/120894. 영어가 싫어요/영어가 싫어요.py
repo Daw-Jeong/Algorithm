daa729e5218bc70c3df1676780bce14c58362f9b
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-05-30 15:55:33 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,11 +1,18 @@
 def solution(numbers):
-    dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
-    to_num = ''
+#     dic = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+#     to_num = ''
     
-    while len(numbers) > 0:
-        for num in dic:
-            if numbers.startswith(num):
-                to_num += dic[num]
-                numbers = numbers[len(num):]
+#     while len(numbers) > 0:
+#         for num in dic:
+#             if numbers.startswith(num):
+#                 to_num += dic[num]
+#                 numbers = numbers[len(num):]
                 
-    return int(to_num)
+#     return int(to_num)
+
+    arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for idx, val in enumerate(arr):
+        numbers = numbers.replace(val, str(idx))
+        
+    return int(numbers)
