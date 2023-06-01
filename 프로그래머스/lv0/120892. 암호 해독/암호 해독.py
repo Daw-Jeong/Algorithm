c7194fbf58404eeb6b2c51d2281e837bad42c293
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-01 22:32:43 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,3 +1,4 @@
 def solution(cipher, code):
 
-    return ''.join([cipher[i] for i in range(len(cipher)) if (i + 1) % code == 0])
+    # return ''.join([cipher[i] for i in range(len(cipher)) if (i + 1) % code == 0])
+    return cipher[code-1::code]
