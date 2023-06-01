add324a29c006fd3b1a9eabe1b0cf68e459a0acb
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-01 22:40:20 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,3 +1,4 @@
 def solution(array):
     
-    return [max(array), array.index(max(array))]
+    # return [max(array), array.index(max(array))]
+    return sorted([[a, i] for i, a in enumerate(array)])[-1]
