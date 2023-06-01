7666aef81568104c6afa4c494854a82188863fab
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-01 22:26:28 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,5 +1,6 @@
 def solution(order):
-    clap = ['3', '6', '9']
+    # clap = ['3', '6', '9']
     order_str = str(order)
     
-    return sum([1 for i in order_str if i in clap])
+    # return sum([1 for i in order_str if i in clap])
+    return order_str.count('3') + order_str.count('6') + order_str.count('9')
