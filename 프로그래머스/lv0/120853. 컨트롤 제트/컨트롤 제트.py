db4643baf106da3fde74dff8f2bfd3a889e4669b
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-01 14:53:18 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,11 @@
+def solution(s):
+    sum = 0
+    li = s.split(' ')
+    
+    for i in range(len(li)):
+        if li[i] != 'Z':
+            sum += int(li[i])
+        else:
+            sum -= int(li[i - 1])            
+    
+    return sum
