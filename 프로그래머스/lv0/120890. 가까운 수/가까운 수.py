9cb9dca4e4dbea5aedcb1f19fadd4fd6d310d4ab
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-01 22:19:00 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,11 +1,15 @@
 def solution(array, n):
-    
+    array.sort()
     li = [abs(n-i) for i in array]
-    d = min(li)
-    # 가장 가까운 수가 여러 개일 경우 더 작은 수를 return 합니다. 이거 고려해야해
-    if li.count(d) > 1:
-        temp = [idx for idx in range(len(li)) if li[idx] == d]
-        return min([array[i] for i in temp])
+    return array[li.index(min(li))]
     
-    else:
-        return array[li.index(d)]
+    
+#     li = [abs(n-i) for i in array]
+#     d = min(li)
+#     # 가장 가까운 수가 여러 개일 경우 더 작은 수를 return 합니다. 이거 고려해야해
+#     if li.count(d) > 1:
+#         temp = [idx for idx in range(len(li)) if li[idx] == d]
+#         return min([array[i] for i in temp])
+    
+#     else:
+#         return array[li.index(d)]
