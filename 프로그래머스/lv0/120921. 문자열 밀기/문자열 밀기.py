2de22ddb9f3be7380e521390cc45326cd36345f5
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-03 00:02:09 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,6 +1,24 @@
 def solution(A, B):
+    
+    doubleA = A+A
+    print(doubleA)
     answer = -1
-    for i in range(0, len(A)):
-        if A[len(A)-i:] + A[:len(A)-i] == B:
-            return i
+    
+    for i in range(1, len(A)):
+        idx = doubleA.find(B, len(A) - i)
+        if idx != -1:
+            answer = idx
+            break
+        
+    print(answer)
+    if answer > 0:
+        answer = len(A) - answer
+        
     return answer
+
+    # answer = -1
+    # for i in range(0, len(A)):
+    #     if A[len(A)-i:] + A[:len(A)-i] == B:
+    #         return i
+    # return answer
+    
