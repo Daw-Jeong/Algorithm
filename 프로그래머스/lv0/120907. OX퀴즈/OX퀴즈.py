f7130d3d89e271758d8e23fe794d7fd63b8ca2a4
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-04 01:01:33 +0900

    [level 0] Title: OX퀴즈, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
@@ -2,16 +2,24 @@ def solution(quiz):
     answer = []
     
     for express in quiz:
-        X, oper, Y, eq, Z = express.split()
+        exp, result = express.split('=')
+        answer.append("O") if eval(exp) == int(result) else answer.append("X")
         
-        if oper == '+':
-            result = 1 if int(X) + int(Y) == int(Z) else 0
-        else:
-            result = 1 if int(X) - int(Y) == int(Z) else 0
-            
-        if result:
-            answer.append("O")
-        else:
-            answer.append("X")
-    
     return answer
+    
+#     answer = []
+    
+#     for express in quiz:
+#         X, oper, Y, eq, Z = express.split()
+        
+#         if oper == '+':
+#             result = 1 if int(X) + int(Y) == int(Z) else 0
+#         else:
+#             result = 1 if int(X) - int(Y) == int(Z) else 0
+            
+#         if result:
+#             answer.append("O")
+#         else:
+#             answer.append("X")
+    
+#     return answer
