52ef700a3e4a819a82df9c7ac8d4edb4f4aa6ae1
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-04 00:42:32 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,2 +1,3 @@
 def solution(num, k):
-    return str(num).find(str(k)) + 1 if str(num).find(str(k)) != -1 else -1
+    # return str(num).find(str(k)) + 1 if str(num).find(str(k)) != -1 else -1
+    return -1 if str(k) not in str(num) else str(num).find(str(k)) + 1
