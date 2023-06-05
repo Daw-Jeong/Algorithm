b29df091e1e4bf94992b3957c351874f80a4bc7c
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-05 16:17:42 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,23 +1,36 @@
 def solution(keyinput, board):
-    answer = [0, 0]
-
-    for keyin in keyinput:
-        if keyin == 'up':
-            answer[1] += 1
-            if answer[1] > board[1] // 2:
-                answer[1] = board[1] // 2
-        elif keyin == 'down':
-            answer[1] -= 1
-            if -answer[1] > board[1] // 2:
-                answer[1] = -(board[1] // 2)
-        elif keyin == 'right':
-            answer[0] += 1
-            if answer[0] > board[0] // 2:
-                answer[0] = board[0] // 2
+    limitX, limitY = board[0]//2, board[1]//2
+    dict = {'up':(0, 1), 'down':(0, -1), 'right':(1, 0), 'left':(-1, 0)}
+    x, y = 0, 0
+    
+    for key in keyinput:
+        dx, dy = dict[key]
+        if abs(x + dx) > limitX or abs(y + dy) > limitY:
+            continue
         else:
-            answer[0] -= 1
-            if -answer[0] > board[0] // 2:
-                answer[0] = -(board[0] // 2)
+            x += dx
+            y += dy
+            
+    return [x, y]
+    
+#     answer = [0, 0]
 
+#     for keyin in keyinput:
+#         if keyin == 'up':
+#             answer[1] += 1
+#             if answer[1] > board[1] // 2:
+#                 answer[1] = board[1] // 2
+#         elif keyin == 'down':
+#             answer[1] -= 1
+#             if -answer[1] > board[1] // 2:
+#                 answer[1] = -(board[1] // 2)
+#         elif keyin == 'right':
+#             answer[0] += 1
+#             if answer[0] > board[0] // 2:
+#                 answer[0] = board[0] // 2
+#         else:
+#             answer[0] -= 1
+#             if -answer[0] > board[0] // 2:
+#                 answer[0] = -(board[0] // 2)
                 
-    return answer
+#     return answer
