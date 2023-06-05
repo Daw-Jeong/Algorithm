21707bc78e3aeb72b0030f5df48922c41201314c
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-06 00:16:10 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,7 +1,8 @@
 def solution(my_string):
     
-    for i in range(len(my_string)):
-        if my_string[i].isalpha():
-            my_string = my_string.replace(my_string[i], ' ')
-            
-    return sum([int(i) for i in my_string.split()])
+    # for i in range(len(my_string)):
+    #     if my_string[i].isalpha():
+    #         my_string = my_string.replace(my_string[i], ' ')
+    intstr = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    
+    return sum([int(i) for i in intstr.split()])
