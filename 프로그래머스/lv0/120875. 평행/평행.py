acb33761e20801adcd16a7223c021c0d7fdceae7
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-08 13:20:08 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,12 +1,9 @@
-def solution(dots):
-    answer = 0
-    if incline(dots[0], dots[1]) == incline(dots[2], dots[3]):
-        return 1
-    if incline(dots[0], dots[2]) == incline(dots[1], dots[3]):
-        return 1
-    if incline(dots[0], dots[3]) == incline(dots[1], dots[2]):
-        return 1
-    return answer
 
-def incline(li1, li2):
-    return (li1[1] - li2[1]) / (li1[0] - li2[0])
+
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    answer1 = (y1 - y2) * (x3 - x4) == (y3 - y4) * (x1 - x2)
+    answer2 = (y1 - y3) * (x2 - x4) == (y2 - y4) * (x1 - x3)
+    answer3 = (y1 - y4) * (x2 - x3) == (y2 - y3) * (x1 - x4)
+            
+    return 1 if answer1 or answer2 or answer3 else 0
