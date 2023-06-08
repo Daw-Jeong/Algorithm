3f86a23a061c7229ea40a8317e64ce9c8480f60f
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-09 01:08:00 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,6 +1,6 @@
 def solution(before, after):
-    beforelist = sorted(list(before))
-    afterlist = sorted(list(after))
+    beforelist = sorted(before)
+    afterlist = sorted(after)
     
     if beforelist == afterlist:
         return 1
