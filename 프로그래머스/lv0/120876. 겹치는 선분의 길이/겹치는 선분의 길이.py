8154d613dbf60ecbf741c9aa2b5bbf2345ef7f64
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-09 00:45:19 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -5,8 +5,7 @@ def solution(lines):
         for i in range(el[0], el[1]):
             li.append(i)
             
-    for l in range(min(lines)[0], max(lines[0][1], lines[1][1], lines[2][1])):
-        
+    for l in range(min(lines)[0], max(lines[0][1], lines[1][1], lines[2][1])):      
         if li.count(l) >= 2:
             answer += 1
 
