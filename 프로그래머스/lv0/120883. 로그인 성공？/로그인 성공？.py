98aafc5a1d50f92ca4e389f4c59db5e5c83418f2
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-09 23:13:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,10 +1,15 @@
 def solution(id_pw, db):
+
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
     
-    for i in db:
-        if i[0] == id_pw[0]:
-            if i[1] == id_pw[1]:
-                return "login"
-            else:
-                return "wrong pw"
+    return "fail"    
+        
+#     for i in db:
+#         if i[0] == id_pw[0]:
+#             if i[1] == id_pw[1]:
+#                 return "login"
+#             else:
+#                 return "wrong pw"
     
-    return "fail"
+#     return "fail"
