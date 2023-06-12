03c07ddf836d03dfac34f8d4e01f4b711f8ea261
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-12 13:01:16 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,20 @@
+def solution(babbling):
+    cando = ["aya", "ye", "woo", "ma"]
+    count = 0
+    for bab in babbling:
+
+        while len(bab) >= 0:
+            
+            if len(bab) == 0:
+                count += 1
+                break          
+            elif len(bab) >= 3 and bab[:3] in cando:
+                bab = bab[3:]
+                
+            elif len(bab) >=2 and bab[:2] in cando:
+                bab = bab[2:]
+            else:
+                break
+                
+    return count
+
