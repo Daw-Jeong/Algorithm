a663f9706ecb149cdfb8f44b2838814dad1745f6
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-13 14:10:58 +0900

    [level 0] Title: 이진수 더하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,13 +1,14 @@
 def solution(bin1, bin2):
-    sumbi = str(int(bin1) + int(bin2))
-    print(sumbi)
-    for i in range(1, len(sumbi) - 1):
-        if int(sumbi[len(sumbi) - i]) >= 2:
-            print('a: ',sumbi[:len(sumbi) - i - 1])
-            print('b: ',str(int(sumbi[len(sumbi) - i - 1]) + 1))
-            print('c: ',sumbi[len(sumbi) - i + 1:])
-            sumbi = sumbi[:len(sumbi) - i - 1] + str(int(sumbi[len(sumbi) - i - 1]) + 1) + str(int(sumbi[len(sumbi) - i]) % 2) + sumbi[len(sumbi) - i + 1:]
+    return bin(int(bin1, 2) + int(bin2, 2))[2:]
+
+# def solution(bin1, bin2):
+#     sumbi = str(int(bin1) + int(bin2))
+
+#     for i in range(1, len(sumbi)):
+#         if int(sumbi[len(sumbi) - i]) >= 2:
+#             sumbi = sumbi[:len(sumbi) - i - 1] + str(int(sumbi[len(sumbi) - i - 1]) + 1) + str(int(sumbi[len(sumbi) - i]) % 2) + sumbi[len(sumbi) - i + 1:]
     
-    if int(sumbi[0]) >= 2:
-        sumbi = '1' + str(int(sumbi[0]) % 2) + sumbi[1:]
-    return sumbi
+#     if int(sumbi[0]) >= 2:
+#         sumbi = '1' + str(int(sumbi[0]) % 2) + sumbi[1:]
+        
+#     return sumbi
