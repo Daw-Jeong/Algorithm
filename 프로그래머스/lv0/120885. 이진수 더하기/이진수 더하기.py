8e3469b2a3188d666986afa41f35ee4ac1289933
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-13 13:32:18 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,11 +1,13 @@
 def solution(bin1, bin2):
     sumbi = str(int(bin1) + int(bin2))
-
-    for i in range(1, len(sumbi)):
+    print(sumbi)
+    for i in range(1, len(sumbi) - 1):
         if int(sumbi[len(sumbi) - i]) >= 2:
+            print('a: ',sumbi[:len(sumbi) - i - 1])
+            print('b: ',str(int(sumbi[len(sumbi) - i - 1]) + 1))
+            print('c: ',sumbi[len(sumbi) - i + 1:])
             sumbi = sumbi[:len(sumbi) - i - 1] + str(int(sumbi[len(sumbi) - i - 1]) + 1) + str(int(sumbi[len(sumbi) - i]) % 2) + sumbi[len(sumbi) - i + 1:]
     
     if int(sumbi[0]) >= 2:
         sumbi = '1' + str(int(sumbi[0]) % 2) + sumbi[1:]
-        
     return sumbi
