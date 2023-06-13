b0316fdac8c62b936878db43b9029005decd4a24
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-06-13 12:35:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,20 +1,29 @@
-def solution(babbling):
-    cando = ["aya", "ye", "woo", "ma"]
-    count = 0
+# def solution(babbling):
+#     cando = ["aya", "ye", "woo", "ma"]
+#     count = 0
     
-    for bab in babbling:
+#     for bab in babbling:
 
-        while len(bab) >= 0:
+#         while len(bab) >= 0:
             
-            if len(bab) == 0:
-                count += 1
-                break          
-            elif len(bab) >= 3 and bab[:3] in cando:
-                bab = bab[3:]    
-            elif len(bab) >=2 and bab[:2] in cando:
-                bab = bab[2:]
-            else:
-                break
+#             if len(bab) == 0:
+#                 count += 1
+#                 break          
+#             elif len(bab) >= 3 and bab[:3] in cando:
+#                 bab = bab[3:]    
+#             elif len(bab) >=2 and bab[:2] in cando:
+#                 bab = bab[2:]
+#             else:
+#                 break
                 
+#     return count
+
+def solution(babbling):
+    count = 0
+    for bab in babbling:
+        for word in [ "aya", "ye", "woo", "ma" ]:
+            bab = bab.replace(word, ' ')
+        if len(bab.strip()) == 0:
+            count += 1
     return count
 
