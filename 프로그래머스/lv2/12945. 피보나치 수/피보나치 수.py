8312e24cad81df61f78d73ebc2d63393a1a2bc72
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-07-22 01:34:54 +0900

    [level 2] Title: 피보나치 수, Time: 117.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,7 @@
 def solution(n):
     temp1 = 0
     temp2 = 1
-    sum = 1
-    for i in range(3, n + 1):
-        temp1 = temp2
-        temp2 = sum   
-        sum = temp1 + temp2
+    for _ in range(2, n + 1):
+        temp1, temp2 = temp2, temp1 + temp2
         
-    return sum % 1234567
+    return temp2 % 1234567
