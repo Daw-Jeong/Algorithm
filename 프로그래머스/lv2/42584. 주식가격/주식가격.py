935e6a270818e95f983eb301e22f1912eaf67a98
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-08-21 14:40:26 +0900

    [level 2] Title: 주식가격, Time: 20.40 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,12 +1,23 @@
 def solution(prices):
-    answer = [0] * len(prices)
+#     answer = [0] * len(prices)
     
-    for i in range(len(prices)):
-        for l in range(i + 1 , len(prices)):
-            answer[i] += 1
-            if prices[l] >= prices[i]:
-                continue
-            else:
-                break
+#     for i in range(len(prices)):
+#         for l in range(i + 1 , len(prices)):
+#             answer[i] += 1
+#             if prices[l] >= prices[i]:
+#                 continue
+#             else:
+#                 break
                 
+#     return answer
+
+    stack = []
+    answer = [0] * len(prices) #[1, 2, 3, 2, 3]
+    for i in range(len(prices)): #0 1 2 3 4
+        while stack != [] and stack[-1][1] > prices[i]:
+            past, _ = stack.pop() #[2,3]
+            answer[past] = i - past #answer[2] = 1
+        stack.append([i, prices[i]]) #[ [0,1] [1,2] [3,2] [4,3] ] 
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
     return answer
