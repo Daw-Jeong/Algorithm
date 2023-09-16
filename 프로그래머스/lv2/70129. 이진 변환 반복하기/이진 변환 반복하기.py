551e05944caa5f231784cf28755217daef76e257
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-09-16 12:30:03 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -2,17 +2,16 @@ def solution(s):
     answer = []
     trial = 0
     cnt = 0
+    
     while len(s) > 1:
         cnt += s.count('0')
         s = s.replace('0','')
-        s = toBinary(s)
+        s = bin(len(s))[2:]
         trial += 1
+        
     answer.append(trial)
     answer.append(cnt)
+    
     return answer
-
-def toBinary(num):
-    temp = len(num)
-    return bin(temp)[2:]
     
     
