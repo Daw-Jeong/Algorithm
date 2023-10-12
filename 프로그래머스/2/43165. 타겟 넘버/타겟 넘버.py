1e4dedd2dfadb818f6151b1bcb1fb0a4961228c7
Daw-Jeong <111163750+Daw-Jeong@users.noreply.github.com>
2023-10-12 11:29:34 +0900

    [level 2] Title: 타겟 넘버, Time: 3.29 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,36 +1,24 @@
 def solution(numbers, target):
-    answer = recursion(numbers, -1, 0, target)
-    return answer
-
-def recursion(numbers, idx, result, target):
-    idx += 1
-    
-    if idx < len(numbers):
-        cnt1 = recursion(numbers, idx, result + numbers[idx], target)
-        cnt2 = recursion(numbers, idx, result - numbers[idx], target)
-        return cnt1 + cnt2
-        
-    else:
-        if result == target:
-            return 1
+    if not numbers and target == 0:
+        return 1
+    elif not numbers:
         return 0
+    else:
+        return solution(numbers[1:], target - numbers[0]) + solution(numbers[1:], target + numbers[0])
 
-    # def solution(numbers, target):
-#     cnt1 = recursion(numbers, 0, numbers[0], target)
-#     cnt2 = recursion(numbers, 0, -numbers[0], target)
-    
-#     return cnt1 + cnt2
+# def solution(numbers, target):
+#     answer = recursion(numbers, -1, 0, target)
+#     return answer
 
 # def recursion(numbers, idx, result, target):
-#     cnt = 0
 #     idx += 1
-#     if idx == len(numbers):
-#         if result == target:
-#             cnt += 1
-#         return cnt
-#     else:
+    
+#     if idx < len(numbers):
 #         cnt1 = recursion(numbers, idx, result + numbers[idx], target)
 #         cnt2 = recursion(numbers, idx, result - numbers[idx], target)
 #         return cnt1 + cnt2
-    
-    
+        
+#     else:
+#         if result == target:
+#             return 1
+#         return 0
